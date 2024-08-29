45ef520af8048e9020b7a1755c3b306ff720c435
ranjan <radhasonk.phy@gmail.com>
2024-08-30 03:37:01 +0530

    Add project files.

---
+++ CustomCurvedPanel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurvedShaped
+{
+    public class CustomCurvedPanel : GroupBox
+    {
+       
+            public CustomCurvedPanel()
+            {
+                this.DoubleBuffered = true;
+                this.TitleBackColor = Color.SteelBlue;
+                this.TitleForeColor = Color.White;
+                this.TitleFont = new Font(this.Font.FontFamily, Font.Size + 8, FontStyle.Bold);
+                this.BackColor = Color.Transparent;
+                this.borderRadius = 1;
+            }
+
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                base.OnPaint(e);
+                GroupBoxRenderer.DrawParentBackground(e.Graphics, this.ClientRectangle, this);
+                var rect = ClientRectangle;
+
+                using (var path = GetCustomShape(this.ClientRectangle))
+                {
+                    e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+                    if (this.BackColor != Color.Transparent)
+                    {
+                        using (var brush = new SolidBrush(BackColor))
+                        {
+                            e.Graphics.FillPath(brush, path);
+                        }
+                    }
+
+                    var clip = e.Graphics.ClipBounds;
+                    e.Graphics.SetClip(rect);
+
+                    e.Graphics.SetClip(clip);
+
+                    using (var pen = new Pen(TitleBackColor, borderRadius))
+                    {
+                        e.Graphics.DrawPath(pen, path);
+                    }
+                }
+            }
+
+            public Color TitleBackColor { get; set; }
+            public int borderRadius { get; set; }
+            public Font TitleFont { get; set; }
+            public Color TitleForeColor { get; set; }
+
+            private GraphicsPath GetCustomShape(Rectangle rect)
+            {
+                GraphicsPath path = new GraphicsPath();
+
+                // Top side - straight line (rectangular)
+                path.StartFigure();
+                path.AddLine(rect.X, rect.Y, rect.Right, rect.Y);  // Top side
+
+                // Right side - straight line downwards
+                path.AddLine(rect.Right, rect.Y, rect.Right, rect.Bottom);  // Right side
+
+                // Bottom side - wavy line from right to left, narrowing
+                int waveHeight = rect.Height / 3; // Height of the wave
+                int narrowWidth = rect.Width / 4; // Width at the narrowest point on the left
+
+                path.AddBezier(
+                    new Point(rect.Right, rect.Bottom),                        // Start at bottom-right corner
+                    new Point(rect.Right - (rect.Width / 3), rect.Bottom - waveHeight),  // Control point 1
+                    new Point(rect.X + narrowWidth, rect.Bottom + waveHeight), // Control point 2
+                    new Point(rect.X, rect.Bottom - waveHeight));              // End at bottom-left, narrow
+
+                // Left side - straight line upwards
+                path.AddLine(rect.X, rect.Bottom - waveHeight, rect.X, rect.Y);  // Left side
+
+                path.CloseFigure();
+                return path;
+            }
+        }
+    }
+    
---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CurvedShaped
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customCurvedPanel21 = new CurvedShaped.CustomCurvedPanel2();
+            this.customCurvedPanel1 = new CurvedShaped.CustomCurvedPanel();
+            this.SuspendLayout();
+            // 
+            // customCurvedPanel21
+            // 
+            this.customCurvedPanel21.BackColor = System.Drawing.Color.Cyan;
+            this.customCurvedPanel21.borderRadius = 1;
+            this.customCurvedPanel21.Location = new System.Drawing.Point(35, 150);
+            this.customCurvedPanel21.Name = "customCurvedPanel21";
+            this.customCurvedPanel21.Size = new System.Drawing.Size(1169, 217);
+            this.customCurvedPanel21.TabIndex = 1;
+            this.customCurvedPanel21.TabStop = false;
+            this.customCurvedPanel21.Text = "customCurvedPanel21";
+            this.customCurvedPanel21.TitleBackColor = System.Drawing.Color.SteelBlue;
+            this.customCurvedPanel21.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold);
+            this.customCurvedPanel21.TitleForeColor = System.Drawing.Color.White;
+            // 
+            // customCurvedPanel1
+            // 
+            this.customCurvedPanel1.BackColor = System.Drawing.Color.Chartreuse;
+            this.customCurvedPanel1.borderRadius = 1;
+            this.customCurvedPanel1.Location = new System.Drawing.Point(2, 3);
+            this.customCurvedPanel1.Name = "customCurvedPanel1";
+            this.customCurvedPanel1.Size = new System.Drawing.Size(797, 126);
+            this.customCurvedPanel1.TabIndex = 0;
+            this.customCurvedPanel1.TabStop = false;
+            this.customCurvedPanel1.Text = "customCurvedPanel1";
+            this.customCurvedPanel1.TitleBackColor = System.Drawing.Color.SteelBlue;
+            this.customCurvedPanel1.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold);
+            this.customCurvedPanel1.TitleForeColor = System.Drawing.Color.White;
+            this.customCurvedPanel1.Enter += new System.EventHandler(this.customCurvedPanel1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 450);
+            this.Controls.Add(this.customCurvedPanel21);
+            this.Controls.Add(this.customCurvedPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomCurvedPanel customCurvedPanel1;
+        private CustomCurvedPanel2 customCurvedPanel21;
+    }
+}
+
